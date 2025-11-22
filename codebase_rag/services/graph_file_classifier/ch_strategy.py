598a50080b63d6539ffb9c21056fe6d4738988ce
sulaga <2500377984@qq.com>
2025-11-22 19:13:33 +0800

    更新子图文件拆分 - ch

--- codebase_rag/services/graph_file_classifier/ch_strategy.py
+++ codebase_rag/services/graph_file_classifier/ch_strategy.py
@@ -2,7 +2,7 @@ from typing import Dict, Set
 from .base_strategy import BaseAntipatternStrategy
 import os
 
-from .utils import find_node_by_entity_and_location, find_related_files_by_relationships
+from .utils import find_node_by_entity_and_location, find_related_files_by_relationships, extract_all_child_methods
 
 
 class CHStrategy(BaseAntipatternStrategy):
@@ -14,13 +14,26 @@ class CHStrategy(BaseAntipatternStrategy):
         return files
 
     def find_indirect_related_files(self, graph_data: Dict, antipattern_json: Dict) -> Set[str]:
-        child_method = antipattern_json["codeSnippets"][0]["childMethod"]
+        child_methods = extract_all_child_methods(antipattern_json)
 
-        child_node = find_node_by_entity_and_location(graph_data, child_method["entity"], child_method["location"])
-        child_node_id = child_node.get("properties", {}).get("id")
+        all_indirect = set()
+        all_direct = set()
 
-        node_ids = set(filter(None, [child_node_id]))
-        indirect = find_related_files_by_relationships(graph_data, node_ids)
-
-        direct_related = self.find_direct_related_files(graph_data, antipattern_json)
-        return indirect - direct_related
+        for child_method in child_methods:
+            child_node = find_node_by_entity_and_location(
+                graph_data,
+                child_method["entity"],
+                child_method["location"]
+            )
+            if not child_node:
+                continue
+            child_node_id = child_node.get("properties", {}).get("id")
+            if not child_node_id:
+                continue
+            indirect = find_related_files_by_relationships(
+                graph_data,
+                node_ids={child_node_id}
+            )
+            all_indirect |= indirect
+        all_direct = self.find_direct_related_files(graph_data, antipattern_json)
+        return all_indirect - all_direct
