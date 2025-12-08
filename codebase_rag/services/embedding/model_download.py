688492ec6afb8e3c19f599334e5610f2524793eb
Allaskas <136561336@qq.com>
2025-12-08 21:40:19 +0800

    无关文件embedding筛选

---
+++ codebase_rag/services/embedding/model_download.py
@@ -0,0 +1,5 @@
+import os
+# // 设置环境变量
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+# // 下载模型
+os.system('huggingface-cli download --resume-download Qwen/Qwen-Embedding-0.6B --local-dir ./Qwen-Embedding-0.6B')
